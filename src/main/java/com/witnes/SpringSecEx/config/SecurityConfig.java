c06ca1de6f77f55e1fc15a6a5604bab6d5c4cae0
witnes sostenes <112706208+nshobairwe@users.noreply.github.com>
2024-10-11 16:24:35 +0300

    customized user by hard coded

--- src/main/java/com/witnes/SpringSecEx/config/SecurityConfig.java
+++ src/main/java/com/witnes/SpringSecEx/config/SecurityConfig.java
@@ -7,6 +7,10 @@ import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
@@ -29,5 +33,27 @@ public class SecurityConfig {
 
         //enable form login
         //http.formLogin(Customizer.withDefaults());
+    }
+
+    //customized user
+    @Bean
+    public UserDetailsService userDetailService(){
+
+        UserDetails user1 = User
+                .withDefaultPasswordEncoder()
+                .username("wit")
+                .password("123")
+                .roles("USER")
+                .build();
+        UserDetails user2 = User
+                .withDefaultPasswordEncoder()
+                .username("witnes")
+                .password("123")
+                .roles("USER")
+                .build();
+
+
+
+        return new InMemoryUserDetailsManager();
     }
 }
